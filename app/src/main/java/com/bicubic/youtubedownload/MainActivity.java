b23196b77dfa142f78c0f2bb85308e7397bcf7ec
arpit999bicubic <androi.bicubics@gmail.com>
2016-11-08 13:57:10 +0530

    Initial commit

---
+++ app/src/main/java/com/bicubic/youtubedownload/MainActivity.java
@@ -0,0 +1,78 @@
+package com.bicubic.youtubedownload;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.commit451.youtubeextractor.YouTubeExtractionResult;
+import com.commit451.youtubeextractor.YouTubeExtractor;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String YOUTUBE_ID = "ea4-5mrpGfE";
+
+    private final YouTubeExtractor mExtractor = YouTubeExtractor.create();
+
+
+    private Callback<YouTubeExtractionResult> mExtractionCallback = new Callback<YouTubeExtractionResult>() {
+        @Override
+        public void onResponse(Call<YouTubeExtractionResult> call, Response<YouTubeExtractionResult> response) {
+            bindVideoResult(response.body());
+        }
+
+        @Override
+        public void onFailure(Call<YouTubeExtractionResult> call, Throwable t) {
+            onError(t);
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        For android youtube extractor library  com.github.HaarigerHarald:android-youtubeExtractor:master-SNAPSHOT
+
+        /*String youtubeLink = "https://www.youtube.com/watch?v=Q5EnvdWKlHw";
+
+        YouTubeUriExtractor ytEx = new YouTubeUriExtractor(this) {
+            @Override
+            public void onUrisAvailable(String videoId, String videoTitle, SparseArray<YtFile> ytFiles) {
+                if (ytFiles != null) {
+                    int itag = 22;
+                    String downloadUrl = ytFiles.get(itag).getUrl();
+                    Toast.makeText(MainActivity.this, "url : "+downloadUrl, Toast.LENGTH_SHORT).show();
+                    Log.i("", "onUrisAvailable: url : "+downloadUrl );
+                }
+            }
+        };
+
+        ytEx.execute(youtubeLink);*/
+
+/*Recommanded Library */
+//        For android youtube extractor library  com.github.Commit451.YouTubeExtractor:youtubeextractor:2.1.0'
+
+        mExtractor.extract(YOUTUBE_ID).enqueue(mExtractionCallback);
+
+
+    }
+
+
+    private void onError(Throwable t) {
+        t.printStackTrace();
+        Toast.makeText(MainActivity.this, "It failed to extract. So sad", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void bindVideoResult(YouTubeExtractionResult result) {
+        Log.d("OnSuccess", "Got a result with the best url: " + result.getBestAvailableQualityVideoUri());
+
+        Toast.makeText(this, "result : " + result.getSd360VideoUri(), Toast.LENGTH_SHORT).show();
+    }
+}
